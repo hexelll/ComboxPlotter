a98381c44d66460b88ded64270c9233e1deed2eb
hexelll <139350367+hexelll@users.noreply.github.com>
2026-05-30 21:05:53 +0200

    Update exemple.lua

--- exemple.lua
+++ exemple.lua
@@ -12,36 +12,35 @@ local MathCharCombinator = require "combox.combinators.MathCharCombinator":new()
 local SquarePixelCombinator = require "combox.combinators.SquarePixelCombinator":new()
 
 local p = Plotter:new{
-    ycoeff=3/2
+    ycoeff=1
 }
 
 local screen = Renderer:new{
     term=mon,
     combinators={
-        SquarePixelCombinator,
-        --MathCharCombinator
+        --SquarePixelCombinator,
+        MathCharCombinator
     }
 }
 
 while true do
     p:plot{
         screen=screen,
+        paddingx=0,
         miny=-2,
         maxy=2,
         graphs={
             p:sample(function(x)
-                return {x,2*math.sin(x+os.clock())}
-            end,-5,5,0.5,"area"),
-            -- p:sample(function(x)
-            --     return {2*math.cos(x+os.clock()),x}
-            -- end,-2,2,0.5,"bar",{axis='y'}),
-            -- p:sample(function(x)
-            --     return {5*math.cos(x+os.clock()),2*math.sin(x+os.clock())}
-            -- end,0,2*math.pi-math.pi/4,0.1,"line",{axis="y"})
+                return {x,2*math.cos(x+os.clock()*2)}
+            end,-5,5,0.5,"bar",{axis='x'}),
+            p:sample(function(x)
+                return {x,2*math.sin(x+os.clock()*2)}
+            end,-5,5,0.5,"line"),
+            
         },
         colors={
             function(u,v)
-                return p.colors.Teal:mix(p.colors.Red,u)
+                return Color(0,(u+os.clock()/5)%1,v)
             end,
             p.colors.Red,
             p.colors.Rosewater
@@ -49,5 +48,5 @@ while true do
         onRender=function(self,s,img)
         end
     }
-    sleep()
+    sleep(0.1)
 end
